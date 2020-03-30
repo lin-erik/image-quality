cb3b859cd2d938e32cce0813314e7265d51a1c94
Erik <elin7780@bths.edu>
2020-03-30 17:51:42 -0400

    Update backend, remove print statements

--- api/app.py
+++ api/app.py
@@ -10,12 +10,11 @@ def get_metrics():
     npfile = numpy.frombuffer(file, numpy.uint8)
     image = cv2.imdecode(npfile, -1)
 
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(image, (19, 19), 0)
+    gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
     laplacian = cv2.Laplacian(gray, cv2.CV_64F).var()
-    blurred = cv2.GaussianBlur(gray, (19, 19), 0)
-    (minVal, maxVal, _, _) = cv2.minMaxLoc(blurred)
+    (minVal, maxVal, _, _) = cv2.minMaxLoc(gray)
 
-    print(laplacian, minVal, maxVal)
     response = { "laplacian": round(laplacian, 2), "minVal": minVal, "maxVal": maxVal }
     return jsonify(response)
 
